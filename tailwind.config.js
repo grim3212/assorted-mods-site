de509638675901cfcc8bc8e39e34861c22973ce7
Dakota Pierone <dakotapierone@yahoo.com>
2023-04-19 07:44:34 -0700

    Update packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,5 @@
 module.exports = {
-  purge: {
-    enabled: process.env.NODE_ENV === 'production',
-    content: ['./index.html', './src/**/*.{vue,ts}'],
-  },
+  content: ['./index.html', './src/**/*.{vue,ts}'],
   plugins: [require('@tailwindcss/typography')],
   future: {
     removeDeprecatedGapUtilities: true,
