0fb58dbf239f792904d369b5b4c8f5d178beb1b4
Dakota Pierone <dakotapierone@yahoo.com>
2023-05-04 07:48:20 -0700

    Add in ultimate fist fragment breakdown

--- src/views/mods/tools/PageUltimateFist.vue
+++ src/views/mods/tools/PageUltimateFist.vue
@@ -17,5 +17,55 @@
       <b>Side note</b>: A number of the configuration values are unused since this does not have other
       tools/armor associated with it and it is not allowed to be enchanted.
     </p>
+
+    <br />
+    <br />
+    <table class="border-collapse border border-slate-500 table-fixed">
+      <caption class="caption-bottom text-sm">
+        The default fragment distrubution across dimensions and chest loot.
+      </caption>
+      <thead>
+        <tr>
+          <th class="border border-slate-500 px-4">Dimension</th>
+          <th class="border border-slate-500 px-4">Loot Tables</th>
+          <th class="border border-slate-500 px-4 w-56">Fragment Pieces</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td class="border border-slate-500 px-4">Overworld</td>
+          <td class="border border-slate-500 px-4"><i>stronghold_corridor</i>, <i>stronghold_crossing</i>, <i>stronghold_library</i>,
+            <i>woodland_mansion</i>, <i>underwater_ruin_big</i>, <i>underwater_ruin_small</i>, <i>ancient_city</i></td>
+          <td class="border border-slate-500 px-4 w-56">
+            <ul class="list-disc list-inside">
+              <li><strong>U</strong> Fragment</li>
+              <li><strong>L</strong> Fragment</li>
+              <li><strong>T</strong> Fragment</li>
+            </ul>
+          </td>
+        </tr>
+        <tr>
+          <td class="border border-slate-500 px-4">Nether</td>
+          <td class="border border-slate-500 px-4"><i>nether_bridge</i>, <i>ruined_portal</i></td>
+          <td class="border border-slate-500 px-4 w-56">
+            <ul class="list-disc list-inside">
+              <li><strong>I</strong> Fragment</li>
+              <li><strong>M</strong> Fragment</li>
+            </ul>
+          </td>
+        </tr>
+        <tr>
+          <td class="border border-slate-500 px-4">End</td>
+          <td class="border border-slate-500 px-4"><i>end_city_treasure</i></td>
+          <td class="border border-slate-500 px-4 w-56">
+            <ul class="list-disc list-inside">
+              <li><strong>A</strong> Fragment</li>
+              <li><strong>E</strong> Fragment</li>
+              <li><strong>Unknown</strong> Fragment</li>
+            </ul>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
