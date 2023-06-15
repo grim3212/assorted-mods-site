816ae9d09a8856277af55f2712123664a71df756
Dakota Pierone <dakotapierone@yahoo.com>
2023-06-15 07:51:40 -0700

    Add some seo support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,26 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
     '@nuxtjs/tailwindcss',
-    '@nuxtjs/eslint-module'
+    '@nuxtjs/eslint-module',
+    'nuxt-link-checker',
+    'nuxt-simple-robots',
+    'nuxt-simple-sitemap'
   ],
   app: {
     rootId: 'app'
-  }
+  },
+  nitro: {
+    prerender: {
+      crawlLinks: true,
+      routes: [
+        '/'
+      ]
+    }
+  },
+  runtimeConfig: {
+    public: {
+      siteUrl: process.env.NUXT_PUBLIC_SITE_URL || 'https://assortedmods.com',
+    },
+    indexable: true,
+  },
 })
